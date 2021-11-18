5e1584876e5614493d36ef9861ff5f9114ff47fa
Alexandra Debartolo <alexandra.debartolo@baruchmail.cuny.edu>
2021-11-18 02:17:28 +0000

    adding another macro for pivots

--- models/dbt_utils_pivot.sql
+++ models/dbt_utils_pivot.sql
@@ -1,10 +1,8 @@
-{{ config (
-    materialized="table"
-
-)}}
-
 select
-{{ dbt_utils.star(from=ref('stg_payment'), except=["payment_method", "status"]) }}
+  status,
+  {{ dbt_utils.pivot(
+      'payment_method',
+      dbt_utils.get_column_values(ref('stg_payment'), 'payment_method')
+  ) }}
 from {{ ref('stg_payment') }}
-where status = 'success'
-
+group by status
